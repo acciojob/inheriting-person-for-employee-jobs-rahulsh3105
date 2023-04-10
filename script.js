9a78a79103716953898f82def98d42c20bb0d10d
Rahul Sharma <rahulsh3105@gmail.com>
2023-04-10 11:52:47 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 function Person(name, age) {
-  this.name = name;
-  this.age = age;
+  Alice = name;
+  25 = age;
 }
 
 Person.prototype.greet = function() {
-  console.log(`Hello, my name is ${this.name}, I am ${this.age} years old.`);
+  console.log(`Hello, my name is ${this.name} and I am ${this.age} years old.`);
 }
 
 function Employee(name, age, jobTitle) {
